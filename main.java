4074b6611d843d106e714fed5fb42123b9937508
Junior Alexander Rivas <jrrivas1@gmail.com>
2022-10-08 23:47:34 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,13 @@
+package paquete;
+
+public class main {
+    public static void main(String[] args) {
+        Coche miCoche=new Coche();
+        miCoche.agregarPuertas();
+        miCoche.agregarPuertas();
+        miCoche.agregarPuertas();
+        miCoche.agregarPuertas();
+
+        miCoche.mostrarPuertas();
+    }
+}
